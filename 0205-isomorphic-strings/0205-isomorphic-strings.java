59b9ec5bdde93a9fdd764fc1a330376ccf3f533a
Daksh Vashistha <151574735+vasis09@users.noreply.github.com>
2026-07-16 12:43:09 +0530

    Time: 25 ms (8.51%), Space: 44.3 MB (11.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,36 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        
+        if(s.length()!=t.length()){
+            return false;
+        }
+    HashMap<Character,Character> map1= new HashMap<>();
+    HashMap<Character,Character> map2 =new HashMap<>();
+    for(int i=0;i<s.length();i++){
+        char ch1=s.charAt(i);
+        char ch2=t.charAt(i);
 
-        if (s.length() != t.length()) {
+        if(map1.containsKey(ch1)){
+        if( map1.get(ch1)!=ch2){
+            return false;
+        }}
+
+        if(map2.containsKey(ch2)){
+            if(map2.get(ch2)!=ch1)
             return false;
         }
 
-        HashMap<Character, Character> mapST = new HashMap<>();
-        HashMap<Character, Character> mapTS = new HashMap<>();
+    map1.put(ch1,ch2);
+    map2.put(ch2,ch1);
 
-        for (int i = 0; i < s.length(); i++) {
 
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-
-            // Check s -> t mapping
-            if (mapST.containsKey(ch1)) {
-                if (mapST.get(ch1) != ch2) {
-                    return false;
-                }
-            }
-
-            // Check t -> s mapping
-            if (mapTS.containsKey(ch2)) {
-                if (mapTS.get(ch2) != ch1) {
-                    return false;
-                }
-            }
-
-            mapST.put(ch1, ch2);
-            mapTS.put(ch2, ch1);
-        }
-
-        return true;
-    }}
+    }
+    return true;
+   
+    }
+}
