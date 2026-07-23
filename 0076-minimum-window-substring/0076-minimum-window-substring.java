62e02ffd9b69ec51bb5e31436045abe495890633
Daksh Vashistha <151574735+vasis09@users.noreply.github.com>
2026-07-23 13:42:19 +0530

    Time: 2 ms (99.78%), Space: 45.4 MB (83.41%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,55 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int right=0; int left=0; int count=0; int start=-1; 
-        int need[]=new int[256];
-        int minlen=Integer.MAX_VALUE;
-    // count frequeny of what we need like count freq of t and then check it on s 
-    for(char ch : t.toCharArray()){
-        need[ch]++;
-    }
-    // now check need of chars in s if we found (char at s )then reduce need(like we dont need now we got )
-    // like if s=ABC t=AB need[A]=1 at 0 b=1 c=1,,,,, if(char at(right))means at s need>0 that means we got our char then reduce the need(coz we got it ) freq[ch(left)]--; as we got it and count ++ like 
-    // now A=0;,b=1,c=1 we found A in s from t now we dont need A anymore so 
+        int right =0; int left =0; int minlen=Integer.MAX_VALUE;
+        int count =0; int start =-1;
+        int need[]= new int [128];
+         // frequency count ;
 
-//our window is valid 
-    while(right<s.length()){
-        char ch = s.charAt(right);
-        if(need[ch]>0){
-            count ++;
-        }
-        need[ch]--;
+         for(char ch : t.toCharArray()){
+            need[ch]++; // we have succesfully counted the freq of t ; 
+         }
 
-        while(count==t.length()){
+         //check if(char is present in s string how will we check that ?  ) by checking the chatAt(s) has freq need[ch]>0; then that means we found char of t in s then we will reduce the need[ch] that we dont need that one in next step as we have already found .... also when we increase count more ++ when we found the char in s ? how 
+         // run a while loop  that track right till last \\\
+         while (right <s.length()) {
+            char ch = s.charAt(right);
+            if(need[ch]>0){ // that means char of t exist in s 
+            count++;
             
-            // minlen update required 
-        if(right-left+1<minlen){
-            minlen=right-left+1;
-            start=left;
-        }
-        //after removing left we will be adding freq if(a) is removed then need =1 from 0 then 
-        char sh=s.charAt(left);
-        need[sh]++;
-        
-        if(need[sh] > 0){
-    count--; 
-        } left++;
-       
-        
+            } 
+            need[ch]--;
+            // if(windwo is valid then shrink it ) when will be valid ? when all char of t prsent in s 
+            // means count ==t.len().  
+
+            while(count==t.length()){
+                if(right-left+1<minlen){
+                    minlen=right-left+1;
+                    start=left;
+                }
+                char sh=s.charAt(left);
+                need[sh]++; 
+                //remove count when we have need and not get that char 
+                if(need[sh]>0){
+                    count --;
+                }
+                left++;
+                
+            }
+            right++;
 
 
-    }
-    right++;
-    }
-    if(start==-1){
-        return "";
-    }
-    return s.substring(start,start+minlen);
-
-    
-
-
-
-
+         }
+         if(start==-1){
+            return "";
+         }
+         return s.substring(start,start+minlen);
 
     }
 }
