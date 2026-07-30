c3fb8af3d11bc6ed318a2266e40a36d881efcfa9
Daksh Vashistha <151574735+vasis09@users.noreply.github.com>
2026-07-30 22:50:41 +0530

    Time: 0 ms (100%), Space: 43 MB (89%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,13 @@
+
+        class Solution {
+    public int minimumPushes(String word) {
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += (i / 8) + 1;
+        }
+
+        return ans;
+    }
+}
+    
