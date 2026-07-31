e2d607300a3cd4257587a67a7f070eb3c722df15
Daksh Vashistha <151574735+vasis09@users.noreply.github.com>
2026-07-31 23:33:59 +0530

    Time: 3 ms (86.07%), Space: 43.1 MB (73.58%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+        if(ch=='('||ch=='['||ch=='{'){
+            st.push(ch);
+        }  else{
+        //if stack is empty means no opening bracket comes 
+        if (st.isEmpty()){
+            return false;
+        }
+        if(ch==')'&& st.peek()=='('){
+            st.pop();
+        }
+        else if(ch=='}'&& st.peek()=='{'){
+            st.pop();
+        }
+        else if(ch==']'&& st.peek()=='['){
+            st.pop();
+        }
+        else{
+            return false;
+        }
+        
+        }
+        
+    }
+    return st.isEmpty();
+}}
